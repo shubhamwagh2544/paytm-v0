a0a42ab28462a426c43be3e45a636f00a4b477e9
shubham.wagh <shubhamwagh2544@gmail.com>
2024-02-13 18:09:09 +0530

    mongoose User schema created

---
+++ backend/db/db.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+// connect to mongo
+mongoose.connect('mongodb+srv://shubhamwagh2544:tv4WAk8LE7DiRBYF@cluster0.dwuqqia.mongodb.net/user')
+
+// define schema
+const UserSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 10
+    },
+    lastname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 10
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 10
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+        maxLength: 10
+    }
+})
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = {
+    User
+}
