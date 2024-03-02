281b7541ca0aa5487e4541feccb44c4e16b90ad9
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-03-02 13:14:42 +0530

    vercel config attemp-1

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ const rootRouter = require('./routes/router')
 app.use(express.json())
 app.use(cors({
     origin: [
+        'https://paytm-v0-backend.vercel.app',
         'https://paytm-v0.vercel.app',
         'http://127.0.0.1:5173',
         'http://localhost:5173'
