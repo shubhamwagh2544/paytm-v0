acfbd0a5968c75153dab72588c85079e7d0e7e60
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-03-02 13:33:50 +0530

    vercel config attempt-3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    preserveSymlinks: true,
+  },
   plugins: [react()],
-})
+  server: {
+    hmr: false,
+  },
+  optimizeDeps: {
+    exclude: [],
+  },
+});
