9911f9085234d266982ef0883bd8a13fae5ce2ea
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-03-02 13:05:52 +0530

    configuring backend for frontend

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ const rootRouter = require('./routes/router')
 app.use(express.json())
 app.use(cors({
     origin: [
+        'https://paytm-v0.vercel.app',
         'http://127.0.0.1:5173',
         'http://localhost:5173'
     ],
@@ -18,6 +19,12 @@ app.use(cors({
 }))
 app.use('/api/v1', rootRouter)
 
+
+app.all("*", (req, res) => {
+    res.status(200).json({
+        message: "This might not be the page you're looking for",
+    });
+});
 
 //server start
 const port = process.env.PORT || 5000
