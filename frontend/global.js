b8e50d30983a670f18451e39b109a6afbea4e5a2
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-03-04 23:31:14 +0530

    vercel config attemp7

--- frontend/global.js
+++ frontend/global.js
@@ -1,4 +1,4 @@
-// const BACKEND_URL = 'http://localhost:3000/api/v1';
+//const BACKEND_URL = 'http://localhost:3000/api/v1';
 const BACKEND_URL = 'https://paytm-v0-backend.vercel.app' 
 
 export default BACKEND_URL;
