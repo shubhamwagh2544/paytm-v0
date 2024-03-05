5d97a62ee045133317b6e9916548399fe02bb544
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-03-05 10:31:28 +0530

    vercel config attempt-11

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -20,17 +20,7 @@ export function SignUpPage() {
         <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8 bg-gray-200 rounded-lg shadow-md py-10 px-8 sm:px-10 lg:px-12">
                 <Heading heading={"Sign Up"} subheading={"Enter your information to create an account"} />
-                <form className="mt-8 space-y-6" onSubmit={async (e) => {
-                    e.preventDefault();
-                    const response = await axios.post(`${BACKEND_URL}/user/signup`, {
-                        username,
-                        firstname,
-                        lastname,
-                        password
-                    });
-                    localStorage.setItem("token", response.data.token);
-                    navigate('/dashboard');
-                }}>
+                <div className="mt-8 space-y-6">
                     <div className="space-y-4">
                         <InputBox label={"Firstname"} placeholder={"firstname"} name={"firstname"} onChange={(e) => {
                             setFirstname(e.target.value);
@@ -46,9 +36,18 @@ export function SignUpPage() {
                         }} />
                     </div>
                     <div className="flex justify-center">
-                        <Button name={"Sign Up"} type="submit" />
+                        <Button name={"Sign Up"} onClick={async (e) => {
+                            const response = await axios.post(`${BACKEND_URL}/user/signup`, {
+                                username,
+                                firstname,
+                                lastname,
+                                password
+                            });
+                            localStorage.setItem("token", response.data.token);
+                            navigate('/dashboard');
+                        }}/>
                     </div>
-                </form>
+                </div>
                 <Warning warning={"Already have an account? "} linkText={"SignIn"} onClick={(e) => {
                     navigate('/signin');
                 }} />
